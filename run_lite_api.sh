b1fa6f3ac34ca35a2dc9086afa57a30f5e607704
Pooyan <xintium@Pooyans-MBP.lan>
2024-03-18 16:27:35 -0500

    fix bash

--- run_lite_api.sh
+++ run_lite_api.sh
@@ -5,4 +5,4 @@ export HUGGINGFACE_API_KEY="your_api_key"
 export TOGETHERAI_API_KEY="your_api_key"
 
 
-python lite_api.py --model "gemini/gemini-pro" --llm
+python lite_api.py --llm --model "gpt-3.5-turbo" --family "gpt" --seed 42 --seed_size 1000 --num_retries 10 --diff_levels [1, 2, 3] --config config.json --experiment ckpts/
